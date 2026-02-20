dc7464b18d8b7f6cf8b8a4c19997eab960dd5c9d
Aaditya Kapoor <aaditya88rock@gmail.com>
2026-02-20 11:27:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import google.generativeai as genai
 
 genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.5-flash")
 
 st.title("🏋️ Personalized Workout & Diet Planner")
 
@@ -29,3 +29,4 @@ if st.button("Generate Plan"):
 
     except Exception as e:
         st.error("API Error: " + str(e))
+
